3909ffd953ccdfb067f7199a73f4d2ae50d93e4a
Thom Seddon <thom@nightworld.com>
2014-01-17 17:51:05 +0000

    Handle and display errors

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ var cronstring = require('cronstring');
 var $input = $('#input');
 var $output = $('#output');
 
+var update = function (val) {
+  val = cronstring(val);
+
+  if (val === false) {
+    if (!$output.hasClass('invalid'))
+      $output.addClass('invalid');
+  } else {
+    $output.removeClass('invalid');
+    $output.text(val);
+  }
+
+  window.location.hash = encodeURIComponent(val);
+};
 
 $input.on('input', function () {
-  var val = $input.val();
-  $output.text(cronstring(val));
-  window.location.hash = encodeURIComponent(val);
+  update($input.val());
 });
 
-if (window.location.hash) {
-  $input.val(decodeURIComponent(window.location.hash.substr(1)));
-  $input.trigger('input');
-}
+if (window.location.hash)
+  update(decodeURIComponent(window.location.hash.substr(1)));
 
